1ce0e802f2ebb6479698214d4ff40a7857711eee
Thiago <thiagodavidpd@gmail.com>
2023-08-10 16:41:11 -0300

    create plane and added dat.gui to make easy to change plane attributes

--- main.js
+++ main.js
@@ -1,12 +1,71 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
+
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10,
+  }
+}
+gui.add(world.plane, 'width', 1, 20).onChange(() => {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments,
+  );
+  const {array} = planeMesh.geometry.attributes.position;
+  for(let i = 0; i < array.length ; i+= 3) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+
+    array[i + 2] = z + Math.random();
+  }
+});
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer();
 
-console.log(scene)
-console.log(camera)
-console.log(renderer)
-
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
+
+camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({ 
+  color: 0xff0000, 
+  side: THREE.DoubleSide,
+  flatShading: true,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+
+scene.add(planeMesh);
+
+const {array} = planeMesh.geometry.attributes.position;
+for(let i = 0; i < array.length ; i+= 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+
+  array[i + 2] = z + Math.random();
+}
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
+
+renderer.render(scene, camera);
